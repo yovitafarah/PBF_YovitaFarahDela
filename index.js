4ca8040a58e1d88d15a7f0e3d3b0478d7de8dfe8
yovitafarah <70680971+yovitafarah@users.noreply.github.com>
2021-03-23 10:39:29 +0700

    Add files via upload
    js7

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+import './index.css';
+import App from './App';
+import * as serviceWorker from 'register-service-worker';
+
+const rootReducer = combineReducers({
+ form: formReducer,
+});
+const store = createStore(rootReducer);
+ReactDOM.render(
+ <Provider store={store}>
+ <App />
+ </Provider>, document.getElementById('root'));
+
+serviceWorker.unregister();
